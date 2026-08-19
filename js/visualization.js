13c87957fcf915e35a01c0dd844bddedd0cff50e
adityarai857 <raiaditya47721@gmail.com>
2026-08-19 22:01:20 +0530

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -1,45 +1,51 @@
-function drawKeypoints(poses) {
-    for (let i = 0; i < poses.length; i++) {
-        const pose = poses[i];
+function setupVisualization() {
+    createCanvas(640, 480);
+}
 
-        for (let j = 0; j < pose.keypoints.length; j++) {
-            const keypoint = pose.keypoints[j];
 
-            if (keypoint.score > 0.5) {
-                drawKeypoint(keypoint);
-            }
+function drawVisualization() {
+    image(video, 0, 0, width, height);
+
+    if (!poses || poses.length === 0) {
+        return;
+    }
+
+    const pose = poses[0].pose;
+
+    if (!pose) {
+        return;
+    }
+
+    // Draw keypoints
+    for (const keypoint of pose.keypoints) {
+        if (keypoint.score > 0.5) {
+            fill(255, 0, 0);
+            noStroke();
+            circle(
+                keypoint.position.x,
+                keypoint.position.y,
+                10
+            );
         }
     }
-}
 
+    // Draw skeleton
+    const skeleton = poses[0].skeleton;
 
-function drawKeypoint(keypoint) {
-    const x = keypoint.position.x;
-    const y = keypoint.position.y;
+    if (skeleton) {
+        stroke(0, 255, 0);
+        strokeWeight(2);
 
-    ellipse(x, y, 10, 10);
-}
+        for (const connection of skeleton) {
+            const partA = connection[0];
+            const partB = connection[1];
 
-
-function drawSkeleton(poses) {
-    for (let i = 0; i < poses.length; i++) {
-        const pose = poses[i];
-
-        for (let j = 0; j < pose.skeleton.length; j++) {
-            const partA = pose.skeleton[j][0];
-            const partB = pose.skeleton[j][1];
-
-            drawSkeletonLine(partA, partB);
+            line(
+                partA.position.x,
+                partA.position.y,
+                partB.position.x,
+                partB.position.y
+            );
         }
     }
-}
-
-
-function drawSkeletonLine(partA, partB) {
-    line(
-        partA.position.x,
-        partA.position.y,
-        partB.position.x,
-        partB.position.y
-    );
 }
